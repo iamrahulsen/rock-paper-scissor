92be5385d1375bc883153c4bb6fae68f07555db3
Rahul Sen <iamrahulsen100@gmail.com>
2021-06-05 13:30:06 +0530

    added all the condition

--- main.c
+++ main.c
@@ -4,6 +4,8 @@
 
 typedef int Player;
 Player you, computer;
+int score_you=0, score_computer=0;
+int count=0;
 
 int menu()
 {
@@ -19,13 +21,78 @@ int menu()
 void setup()
 {
     generate_again:
-    computer = rand()%4;
+    computer = (rand()%4);
     if(computer == 0)
     {
         goto generate_again;   
     }
 
     you = menu();
+}
+
+void logic()
+{
+    switch (you)
+    {
+        case 1:
+            if(computer == 1)
+            {
+                printf("Game draw\n");
+                printf("Both of you have choosen ROCK");
+            }
+            else if(computer == 2)
+            {
+                printf("Computer won\n");
+                printf("You = ROCK \t Computer = PAPER");
+            }
+            else          
+            {
+                printf("You won\n");
+                printf("You = ROCK \t Computer = SCISSOR");
+            }
+            break;
+        case 2:
+            if(computer == 1)
+            {
+                printf("You won\n");
+                printf("You = PAPER \t Computer = ROCK");
+            }
+            else if(computer == 2)
+            {
+                printf("Game draw\n");
+                printf("Both of you have choosen PAPER");
+            }
+            else          
+            {
+                printf("Computer won\n");
+                printf("You = PAPER \t Computer = SCISSOR");
+            }
+            break;
+        case 3:
+            if(computer == 1)
+            {
+                printf("Computer won\n");
+                printf("You = SCISSOR \t Computer = ROCK");
+            }
+            else if(computer == 2)
+            {
+                printf("You won\n");
+                printf("You = SCISSOR \t Computer = PAPER");
+            }
+            else          
+            {
+                printf("Game draw\n");
+                printf("Both of you have choosen SCISSOR");
+            }
+            break;
+        case 4:
+            exit(0);
+            break;
+    
+        default:
+            printf("Invalid choice, please try again ");
+
+    }
 }
 
 void game_name()
@@ -35,7 +102,15 @@ void game_name()
 
 int main()
 {
-    game_name();
-    setup();
+    do
+    {
+        game_name();
+        setup();
+        logic();
+    } while (0);
+    
+    
+
+    printf("Do you want to play again");
     return 0;
 }
